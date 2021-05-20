080b20a702f3cd0436c0c2b118d66a1d29bf9b00
Zack2kl <41578376+Zack2kl@users.noreply.github.com>
2021-05-20 15:34:30 -0400

    1.0
    Adds a button to scoresaber leaderboard pages to take you directly to the download for that map

---
+++ Search Map on BeatSaver/index.js
@@ -0,0 +1,37 @@
+function OnScoreSaber() {
+    const tElement = document.getElementsByClassName('title is-5')[0];
+    const infoElement = document.getElementsByClassName('column')[0];
+    const info = infoElement.innerText.split('\n');
+
+    const AutoOpen = document.createElement('button');
+    let mapId = '';
+
+    for (let el in info) {
+        if (info[el].indexOf('ID: ') != -1) {
+            mapId = info[el].replace('ID: ', '');
+            break;
+        }
+    }
+
+    AutoOpen.innerText = 'Go to Download Page';
+    AutoOpen.onclick = () => window.open(`https://beatsaver.com/search?auto_open=1&q=${encodeURI(mapId)}`);
+    tElement.insertBefore(AutoOpen, tElement.Node);
+}
+
+function OnBSaver() {
+    const results = document.getElementsByClassName('beatmap-result');
+    const loop = () => {
+        if (results[0] === undefined)
+            setTimeout(loop, 50);
+        else 
+            window.location.href = 'https://beatsaver.com/beatmap/' + results[0].id;
+    }
+
+    loop();
+}
+
+if (window.location.host == 'scoresaber.com') {
+    OnScoreSaber();
+} else {
+    OnBSaver();
+}
